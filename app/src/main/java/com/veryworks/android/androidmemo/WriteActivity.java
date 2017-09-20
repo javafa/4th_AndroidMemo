d13131e04af3559770db94da68dca4575644634a
javafa <nigne2016@gmail.com>
2017-09-20 11:05:27 +0900

    add writeActivity

--- app/src/main/java/com/veryworks/android/androidmemo/WriteActivity.java
+++ app/src/main/java/com/veryworks/android/androidmemo/WriteActivity.java
@@ -5,6 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.veryworks.android.androidmemo.domain.Memo;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class WriteActivity extends AppCompatActivity {
 
@@ -21,18 +27,42 @@ public class WriteActivity extends AppCompatActivity {
         initListener();
     }
 
-    private void write(){
-        // 내용을 등록하고, 목록으로 이동
+    /**
+     * 내용을 파일에 작성
+     * - 파일쓰기
+     *   내부저장소 - Internal : 개별앱만 접근가능, 파일탐색기에서 보이지 않는다.
+     *   외부저장소 - External : 모든앱이 접근가능 > 권한 필요
+     */
+    private static final String DIR_INTR = "/data/data/com.veryworks.android.androidmemo/files/";
 
-        // 현재 액티비티를 종료한다.
+    private Memo getMemoFromScreen(){
+        Memo memo = new Memo();
+        memo.setNo(1);
+        memo.setTitle(editTitle.getText().toString());
+        memo.setAuthor(editAuthor.getText().toString());
+        memo.setContent(editContent.getText().toString());
+        memo.setDatetime(System.currentTimeMillis());
+        return memo;
+    }
+    private void write(Memo memo){
+        try {
+            String filename = System.currentTimeMillis() + ".txt";
+            File file = new File(DIR_INTR + filename);
+            // 1. 스트림을 열고
+            FileOutputStream fos = new FileOutputStream(file);
+            // 2. 내용을 쓴다.
+            fos.write(memo.toBytes());
+        }catch(Exception e){
+            Toast.makeText(this, "에러:"+e.toString(), Toast.LENGTH_SHORT).show();
+        }
         finish();
     }
-
     private void initListener() {
         btnPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                write();
+                Memo memo = getMemoFromScreen();
+                write(memo);
             }
         });
     }
--- app/src/main/java/com/veryworks/android/androidmemo/domain/Memo.java
+++ app/src/main/java/com/veryworks/android/androidmemo/domain/Memo.java
@@ -5,9 +5,75 @@ package com.veryworks.android.androidmemo.domain;
  */
 
 public class Memo {
-    int no;
-    String title;
-    String author;
-    String content;
-    long datetime;
+    private int no;
+    private String title;
+    private String author;
+    private String content;
+    private long datetime;
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(long datetime) {
+        this.datetime = datetime;
+    }
+
+    public String toString(){
+        StringBuilder result = new StringBuilder();
+        result.append("no:^:").append(no).append("\n");
+        result.append("title:^:").append(title).append("\n");
+        result.append("author:^:").append(author).append("\n");
+        result.append("datetime:^:").append(datetime).append("\n");
+        result.append("content:^:").append(content).append("\n");
+        return result.toString();
+    }
+
+    public byte[] toBytes(){
+        return toString().getBytes(); // 문자열에서 바이트배열로 변환
+    }
 }
+
+/*
+글번호 :^: 1
+제목 :^: 제목입니다
+작성자 :^: 홍길도
+작성일자 :^: 2017/09/20 10:55
+내용 :^: 라ㅣㅓㅣㅏㅁㅇㄹ
+ㅇ미라ㅓㄴㅇㅁㄹ
+ㅏㅇ럼니
+이ㅏ러ㅣ
+ㅣㅓㄹㄴㅁ
+ */
